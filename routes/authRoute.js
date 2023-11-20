051cbf4da6452a81fbeca8e8a255e0ac02a62286
bharatgl <goyalbharat9977@gmail.com>
2023-11-20 22:14:04 +0530

    feat:added getAuser

--- routes/authRoute.js
+++ routes/authRoute.js
@@ -5,14 +5,15 @@ const {
   getallUsers,
   getaUser,
   deleteaUser,
-  updatedUser
+  updatedUser,
 } = require("../controller/userCtrl");
+const authMiddleware = require("../middlewares/authMiddleware");
 const router = express.Router();
 
 router.post("/register", createUser);
 router.post("/login", loginUserControl);
 router.get("/all-users", getallUsers);
-router.get("/:id", getaUser);
+router.get("/:id", authMiddleware, getaUser);
 router.delete("/:id", deleteaUser);
-router.put("/:id",updatedUser)
+router.put("/:id", updatedUser);
 module.exports = router;
